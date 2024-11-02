a434fae20139722c0f5c4af93696088e3a408c18
Nomura <Nomura@DESKTOP-MAI4SL2>
2024-11-02 20:22:33 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿
+namespace MauiAppHotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400; //largura janela
+            window.Height = 600; //altura janela
+            return window;
+        }
+    }
+}
